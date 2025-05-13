7074bc06181dd15d2d3d4d4f5499b0fa7499487b
AbdulBasit <abdulbasit@Admins-Mac-mini.localdomain>
2025-05-14 01:50:06 +0500

    server-less

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 const express = require("express");
+const serverless = require("serverless-http");
 const app = express();
 const bodyParser = require("body-parser");
 const port = 4000;
@@ -12,6 +13,8 @@ require("./src/utils/planCrone");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", routeRouter);
-app.listen(port, () => {
-  console.log(`Server is Runing On the port${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is Runing On the port${port}`);
+// });
+module.exports = app;
+module.exports.handler = serverless(app);
